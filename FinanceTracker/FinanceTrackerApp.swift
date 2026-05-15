882ca928d66375c97623a9b6fbdfb7ea0dc3d930
Jayesh Lokhande <jlokhande46@gmail.com>
2026-05-16 00:35:19 +0530

    Initial Commit

---
+++ FinanceTracker/FinanceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinanceTrackerApp.swift
+//  FinanceTracker
+//
+//  Created by Jayesh Lokhande on 16/05/26.
+//
+
+import SwiftUI
+
+@main
+struct FinanceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
